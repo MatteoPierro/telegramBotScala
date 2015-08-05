832014538556615023e215a14ae90488c8c43fc4
Matteo <pierromatteo@gmail.com>
2015-08-05 18:34:46 +0200

    confirm received messages

--- src/main/scala/com/elios85/telegrambot/actor/TelegramBotActor.scala
+++ src/main/scala/com/elios85/telegrambot/actor/TelegramBotActor.scala
@@ -9,7 +9,7 @@ import scala.concurrent.duration._
 class TelegramBotActor(telegramApi: TelegramAPI) extends Actor with ActorLogging{
   import context.dispatcher
   
-  context.system.scheduler.schedule(0 second, 10 seconds) {
+  context.system.scheduler.schedule(0 second, 1 second) {
     self ! Update
   }
   
@@ -17,6 +17,7 @@ class TelegramBotActor(telegramApi: TelegramAPI) extends Actor with ActorLogging
   
   def receiveBotMessage(lastUpdateId: Int):Receive ={
     case Update => handleTelegramResponse(telegramApi.getUpdates(lastUpdateId, 10)) 
+    case Ack(updateId) => context become receiveBotMessage(updateId + 1)
     case Message(chatId, text) => telegramApi.sendMessage(chatId, text)
     case _ =>
   }
@@ -24,9 +25,11 @@ class TelegramBotActor(telegramApi: TelegramAPI) extends Actor with ActorLogging
   private def handleTelegramResponse(response: Response) = response match {
     case Response(true, messages) => 
       for(message <- messages iterator){
+        val updateId = message.getAsJsonObject.get("update_id").getAsInt
         val messageObject = message.getAsJsonObject.get("message").getAsJsonObject
         val text = messageObject get("text") getAsString
         val chatId = messageObject.get("chat").getAsJsonObject().get("id").getAsInt
+        self ! Ack(updateId)
         self ! Message(chatId, text)
       }
       
@@ -34,5 +37,6 @@ class TelegramBotActor(telegramApi: TelegramAPI) extends Actor with ActorLogging
   }
 }
 
+case class Ack(updateId:Int)
 case class Update()
 case class Message(chatId:Integer, text: String)
